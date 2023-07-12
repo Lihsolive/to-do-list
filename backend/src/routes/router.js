977a9cc06e31a7b99c11cba95e1ed50c2adccaf5
Lihsolive <liliamoliveira13@gmail.com>
2023-07-11 21:31:21 -0300

    feat: configuring .env

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/tasks", (req, res) => res.status(200).send("Router funcionando!"));
+//router.get("/tasks", (req, res) => res.status(200).send("Router funcionando!"));
 
 module.exports = router;
